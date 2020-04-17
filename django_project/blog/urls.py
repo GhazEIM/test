b49862e4680c1a93607c224c1b6d0986b73cd154
Ghassan <ghassan@eimgr.com>
2020-04-17 15:44:40 -0400

    this is the initial commit

---
+++ django_project/blog/urls.py
@@ -0,0 +1,23 @@
+# We created this file similar to Django url file
+
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+from . import views
+
+urlpatterns = [
+    # path('', views.home, name='blog-home'), #This used the Function based view and was later changed to use the Class based view (see <blog> views.py)
+    path('', PostListView.as_view(), name = 'blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name = 'user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'post-delete'),
+    path('about/', views.about, name='blog-about'),
+]
